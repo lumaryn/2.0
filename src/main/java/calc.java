beca14fd0dfc55375d7da09456134c52c7a4ab6c
pochter123 <pochter123@gmail.com>
2019-07-23 21:03:42 +0400

    21

--- src/main/java/calc.java
+++ src/main/java/calc.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class calc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Выберите операцию:  *  /  -  +");
+        char x = sc.next().charAt(0);
+        System.out.println("Введите 1 число");
+        double a = sc.nextDouble();
+        System.out.println("Введите 2 число");
+        double b = sc.nextDouble();
+        if (x == '*'){
+        System.out.printf("mult= "+(a*b));}
+        if (x == '/'){
+            System.out.printf("division= "+(a/b));}
+        if (x == '+'){
+            System.out.printf("add= "+ (a+b));}
+        if (x == '-'){
+            System.out.printf("subtr= "+ (a-b));}
+    }
 }
