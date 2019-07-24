99327727f8d56817bea6050939a84e86418611b3
pochter123 <pochter123@gmail.com>
2019-07-24 11:43:30 +0400

    2

--- src/main/java/calc.java
+++ src/main/java/calc.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+/**
+ *created by aplana on 23.07
+ *@author aplana
+ **/
+
 public class calc {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -9,13 +14,14 @@ public class calc {
         double a = sc.nextDouble();
         System.out.println("Введите 2 число");
         double b = sc.nextDouble();
+        sc.close();
         if (x == '*'){
-        System.out.printf("mult= "+(a*b));}
+        System.out.printf("mult = %.4f",(a*b));}
         if (x == '/'){
-            System.out.printf("division= "+(a/b));}
+            System.out.printf("division = %.4f",(a/b));}
         if (x == '+'){
-            System.out.printf("add= "+ (a+b));}
+            System.out.printf("add = %.4f",(a+b));}
         if (x == '-'){
-            System.out.printf("subtr= "+ (a-b));}
+            System.out.printf("subtr = %.4f",(a-b));}
     }
 }
