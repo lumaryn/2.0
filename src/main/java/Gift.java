51e93bbe6c1f18a53c8e8e7a7fbdda07cdd1a155
pochter123 <pochter123@gmail.com>
2019-07-31 19:07:18 +0400

    2

--- src/main/java/Gift.java
+++ src/main/java/Gift.java
@@ -1,2 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Gift {
+    public static void main(String[] args)  {
+        gift();
+    }
+
+    public static void gift() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Нажмите соответствующую цифру, чтобы добавить сладость в подарок:");
+        System.out.println("'1' - конфеты \"Красная шапочка\"");
+        System.out.println("'2' - конфеты \"Ананас\"");
+        System.out.println("'3' - леденец \"Чупа-чупс\"");
+        System.out.println("'4' - закончить выбор");
+    }
 }
--- src/main/java/Jellybean.java
+++ src/main/java/Jellybean.java
@@ -1,2 +1,39 @@
 public class Jellybean {
+    private String name;
+    private Integer weight;
+    private Integer price;
+    private String option;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
 }
+
